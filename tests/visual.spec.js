ada136150685a433546ab3da89fccaac6868e337
wynnflower <kazehanapiko@gmail.com>
2026-04-26 14:28:00 +0700

    updating visual test script

--- tests/visual.spec.js
+++ tests/visual.spec.js
@@ -6,5 +6,8 @@ test('Visual Comparison Test - Halaman Login', async ({ page }) => {
     //  Visual Testing
     // 'login-page.png' adalah nama file referensi kita
     await page.locator('[data-test="username"]').fill('piko-id');
-    await expect(page).toHaveScreenshot('login-page.png');
+    // await expect(page).toHaveScreenshot('login-page.png');
+    await expect(page).toHaveScreenshot('login-page.png', {
+        mask: [page.locator('.iklan-banner')] // Kotak iklan akan ditutup warna pink saat dibandingkan
+    });
 });
